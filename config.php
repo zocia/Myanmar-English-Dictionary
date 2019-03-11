6a9f9f7a06674d9c3cfcb54159d23d4a2dbfa89e
BJ <necessarylion1@gmail.com>
2019-03-11 06:23:25 -0700

    update again

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-//$conn = new mysqli('localhost','root','','dictionary');
+$conn = new mysqli('localhost','root','','dictionary');
 //$conn = new mysqli('sql106.epizy.com','epiz_22276166','ajaypitlaal','epiz_22276166_dictionary');
 mysqli_set_charset ($conn, 'utf8');
 ?>
